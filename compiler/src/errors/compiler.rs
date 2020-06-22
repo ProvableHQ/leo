eeaf1b4ae865b138023299855cf100496ce27855
collin <collin.chin@berkeley.edu>
2020-06-22 16:39:33 -0700

    remove unused compiler errors

--- compiler/src/errors/compiler.rs
+++ compiler/src/errors/compiler.rs
@@ -1,18 +1,11 @@
 use crate::errors::{FunctionError, ImportError};
-use leo_ast::{ParserError, SyntaxError};
+use leo_ast::ParserError;
 use leo_inputs::InputParserError;
-use leo_types::IntegerError;
 
-use std::{io, path::PathBuf};
+use std::path::PathBuf;
 
 #[derive(Debug, Error)]
 pub enum CompilerError {
-    #[error("creating: {}", _0)]
-    Creating(io::Error),
-
-    #[error("Attempt to access current directory failed - {:?}", _0)]
-    DirectoryError(io::Error),
-
     #[error("{}", _0)]
     ImportError(#[from] ImportError),
 
@@ -20,16 +13,10 @@ pub enum CompilerError {
     InputParserError(#[from] InputParserError),
 
     #[error("{}", _0)]
-    IntegerError(#[from] IntegerError),
-
-    #[error("{}", _0)]
     FunctionError(#[from] FunctionError),
 
     #[error("Cannot read from the provided file path - {:?}", _0)]
     FileReadError(PathBuf),
-
-    #[error("Syntax error. Cannot parse the file")]
-    FileParsingError,
 
     #[error("Main function not found")]
     NoMain,
@@ -39,13 +26,4 @@ pub enum CompilerError {
 
     #[error("{}", _0)]
     ParserError(#[from] ParserError),
-
-    #[error("{}", _0)]
-    SyntaxError(#[from] SyntaxError),
-
-    #[error("Unable to construct abstract syntax tree")]
-    SyntaxTreeError,
-
-    #[error("writing: {}", _0)]
-    Writing(io::Error),
 }
