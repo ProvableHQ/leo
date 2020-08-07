599c886d2358f0c7830b85fbd526ffc41123d872
collin <collin.chin@berkeley.edu>
2020-08-07 15:05:00 -0700

    add formatting for unary expression

--- ast/src/expressions/unary_expression.rs
+++ ast/src/expressions/unary_expression.rs
@@ -3,6 +3,7 @@ use crate::{ast::Rule, expressions::Expression, operations::UnaryOperation, Span
 use pest::Span;
 use pest_ast::FromPest;
 use serde::Serialize;
+use std::fmt;
 
 #[derive(Clone, Debug, FromPest, PartialEq, Serialize)]
 #[pest_ast(rule(Rule::expression_unary))]
@@ -12,4 +13,10 @@ pub struct UnaryExpression<'ast> {
     #[pest_ast(outer())]
     #[serde(with = "SpanDef")]
     pub span: Span<'ast>,
+}
+
+impl<'ast> fmt::Display for UnaryExpression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.operation, self.expression)
+    }
 }
--- ast/src/operations/unary_operation.rs
+++ ast/src/operations/unary_operation.rs
@@ -2,12 +2,22 @@ use crate::ast::Rule;
 
 use pest_ast::FromPest;
 use serde::Serialize;
+use std::fmt;
 
 #[derive(Clone, Debug, FromPest, PartialEq, Serialize)]
 #[pest_ast(rule(Rule::operation_unary))]
 pub enum UnaryOperation {
     Minus(Minus),
     Not(Not),
+}
+
+impl fmt::Display for UnaryOperation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UnaryOperation::Minus(_) => write!(f, "-"),
+            UnaryOperation::Not(_) => write!(f, "!"),
+        }
+    }
 }
 
 #[derive(Clone, Debug, FromPest, PartialEq, Serialize)]
