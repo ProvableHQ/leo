f063d989ffdd8c9c3bb7e6df30d8d11637581f63
howardwu <howardwu@berkeley.edu>
2020-08-21 04:43:28 -0700

    Update to leo_lang

--- leo/main.rs
+++ leo/main.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
-use leo::{cli::*, commands::*, config::Config, errors::CLIError};
+use leo_lang::{cli::*, commands::*, config::Config, errors::CLIError};
 
 use clap::{App, AppSettings, Arg};
 
