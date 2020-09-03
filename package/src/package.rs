1ab7c49e725633d2accf2836a6ebbc0e625476f3
raychu86 <raychu86@gmail.com>
2020-09-02 18:57:33 -0700

    Implement remove imported package

--- package/src/package.rs
+++ package/src/package.rs
@@ -1,5 +1,22 @@
+// Copyright (C) 2019-2020 Aleo Systems Inc.
+// This file is part of the Leo library.
+
+// The Leo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The Leo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
+
 use crate::{
     errors::PackageError,
+    imports::ImportsDirectory,
     inputs::{InputFile, InputsDirectory, StateFile},
     root::{Gitignore, Manifest, README},
     source::{LibraryFile, MainFile, SourceDirectory},
@@ -167,7 +184,7 @@ impl Package {
     }
 
     /// Removes the package at the given path
-    pub fn remove_package(_package_name: &str) -> Result<(), PackageError> {
-        unimplemented!()
+    pub fn remove_imported_package(package_name: &str, path: &PathBuf) -> Result<(), PackageError> {
+        Ok(ImportsDirectory::remove_import(path, package_name)?)
     }
 }
