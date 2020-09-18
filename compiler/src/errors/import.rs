557fccd9dab77381aca6c4e1d88d218df1835688
collin <collin.chin@berkeley.edu>
2020-09-18 12:35:27 -0700

    remove old import parser code from compiler

--- compiler/src/errors/import.rs
+++ compiler/src/errors/import.rs
@@ -14,11 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
-use leo_ast::ParserError;
-use leo_typed::{Error as FormattedError, Identifier, ImportSymbol, Span};
-
 use leo_core::LeoCoreError;
-use std::{io, path::PathBuf};
+use leo_typed::{Error as FormattedError, Identifier, ImportSymbol, Span};
 
 #[derive(Debug, Error)]
 pub enum ImportError {
@@ -27,63 +24,11 @@ pub enum ImportError {
 
     #[error("{}", _0)]
     LeoCoreError(#[from] LeoCoreError),
-
-    #[error("{}", _0)]
-    ParserError(#[from] ParserError),
 }
 
 impl ImportError {
     fn new_from_span(message: String, span: Span) -> Self {
         ImportError::Error(FormattedError::new_from_span(message, span))
-    }
-
-    fn new_from_span_with_path(message: String, span: Span, path: PathBuf) -> Self {
-        ImportError::Error(FormattedError::new_from_span_with_path(message, span, path))
-    }
-
-    pub fn conflicting_imports(identifier: Identifier) -> Self {
-        let message = format!("conflicting imports found for `{}`", identifier.name);
-
-        Self::new_from_span(message, identifier.span)
-    }
-
-    pub fn convert_os_string(span: Span) -> Self {
-        let message = format!("failed to convert file string name, maybe an illegal character?");
-
-        Self::new_from_span(message, span)
-    }
-
-    pub fn current_directory_error(error: io::Error) -> Self {
-        let span = Span {
-            text: "".to_string(),
-            line: 0,
-            start: 0,
-            end: 0,
-        };
-        let message = format!("compilation failed trying to find current directory - {:?}", error);
-
-        Self::new_from_span(message, span)
-    }
-
-    pub fn directory_error(error: io::Error, span: Span, path: PathBuf) -> Self {
-        let message = format!("compilation failed due to directory error - {:?}", error);
-
-        Self::new_from_span_with_path(message, span, path)
-    }
-
-    pub fn star(path: PathBuf, span: Span) -> Self {
-        let message = format!("cannot import `*` from path `{:?}`", path);
-
-        Self::new_from_span(message, span)
-    }
-
-    pub fn expected_lib_file(entry: String, span: Span) -> Self {
-        let message = format!(
-            "expected library file`{}` when looking for symbol `{}`",
-            entry, span.text
-        );
-
-        Self::new_from_span(message, span)
     }
 
     pub fn unknown_package(identifier: Identifier) -> Self {
