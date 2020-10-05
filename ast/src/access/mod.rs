4f46dd2982bce516c6279abbf35967bc0aa5896d
ljedrz <ljedrz@gmail.com>
2020-10-05 16:34:06 +0200

    clippy: allow module_inception
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- ast/src/access/mod.rs
+++ ast/src/access/mod.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
+#![allow(clippy::module_inception)]
+
 pub mod access;
 pub use access::*;
 
