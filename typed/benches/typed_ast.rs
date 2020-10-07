39cd98d2c0eb409e62fda97edc0237d6c3705ac6
ljedrz <ljedrz@gmail.com>
2020-10-07 13:38:11 +0200

    bench: extend the LeoTypedAst benchmarks
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- typed/benches/typed_ast.rs
+++ typed/benches/typed_ast.rs
@@ -17,22 +17,81 @@
 use leo_ast::{errors::ParserError, files::File, LeoAst};
 use leo_typed::LeoTypedAst;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::path::{Path, PathBuf};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::{
+    path::{Path, PathBuf},
+    time::Duration,
+};
 
-fn leo_typed_ast<'ast>(ast: &LeoAst<'ast>) {
-    let typed_ast = LeoTypedAst::new("leo_typed_tree", &ast);
-    black_box(typed_ast);
+fn leo_typed_ast<'ast>(ast: &LeoAst<'ast>) -> LeoTypedAst {
+    LeoTypedAst::new("leo_typed_tree", &ast)
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let filepath = Path::new("./main.leo").to_path_buf();
-    // let program_string = &LeoAst::load_file(&filepath).unwrap();
-    let program_string = include_str!("./main.leo");
+fn bench_big_if_else(c: &mut Criterion) {
+    let filepath = Path::new("./big_if_else.leo").to_path_buf();
+    let program_string = include_str!("./big_if_else.leo");
     let ast = LeoAst::new(&filepath, program_string).unwrap();
 
-    c.bench_function("LeoTypedAst::new", |b| b.iter(|| leo_typed_ast(black_box(&ast))));
+    c.bench_function("LeoTypedAst::big_if_else", |b| b.iter(|| leo_typed_ast(&ast)));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_big_ternary(c: &mut Criterion) {
+    let filepath = Path::new("./big_ternary.leo").to_path_buf();
+    let program_string = include_str!("./big_ternary.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::big_ternary", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+fn bench_big_circuit(c: &mut Criterion) {
+    let filepath = Path::new("./big_circuit.leo").to_path_buf();
+    let program_string = include_str!("./big_circuit.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::big_circuit", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+fn bench_long_expr(c: &mut Criterion) {
+    let filepath = Path::new("./long_expr.leo").to_path_buf();
+    let program_string = include_str!("./long_expr.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::long_expr", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+fn bench_long_array(c: &mut Criterion) {
+    let filepath = Path::new("./long_array.leo").to_path_buf();
+    let program_string = include_str!("./long_array.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::long_array", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+fn bench_many_foos(c: &mut Criterion) {
+    let filepath = Path::new("./many_foos.leo").to_path_buf();
+    let program_string = include_str!("./many_foos.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::many_foos", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+fn bench_many_assigns(c: &mut Criterion) {
+    let filepath = Path::new("./many_assigns.leo").to_path_buf();
+    let program_string = include_str!("./many_assigns.leo");
+    let ast = LeoAst::new(&filepath, program_string).unwrap();
+
+    c.bench_function("LeoTypedAst::many_assigns", |b| b.iter(|| leo_typed_ast(&ast)));
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(200).measurement_time(Duration::from_secs(10)).nresamples(200_000);
+    targets = bench_big_circuit,
+    bench_long_expr,
+    bench_big_if_else,
+    bench_big_ternary,
+    bench_long_array,
+    bench_many_assigns,
+    bench_many_foos,
+);
 criterion_main!(benches);
