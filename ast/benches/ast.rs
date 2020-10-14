07743b2d7577e8d5eaab15ea83e8c0ad7e442418
ljedrz <ljedrz@gmail.com>
2020-10-14 13:08:13 +0200

    bench: change the AST bench to a much more complex one
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- ast/benches/ast.rs
+++ ast/benches/ast.rs
@@ -14,24 +14,20 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
-use leo_ast::{errors::ParserError, files::File, LeoAst};
+use leo_ast::LeoAst;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::path::{Path, PathBuf};
 
-fn leo_ast<'ast>(filepath: &'ast PathBuf, program_string: &'ast str) {
-    let result = LeoAst::<'ast>::new(filepath, program_string).unwrap();
-    black_box(result);
+fn leo_ast<'ast>(filepath: &'ast PathBuf, program_string: &'ast str) -> LeoAst<'ast> {
+    LeoAst::<'ast>::new(filepath, program_string).unwrap()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let filepath = Path::new("./main.leo").to_path_buf();
-    // let program_string = &LeoAst::load_file(&filepath).unwrap();
     let program_string = include_str!("./main.leo");
 
-    c.bench_function("LeoAst::new", |b| {
-        b.iter(|| leo_ast(black_box(&filepath), black_box(program_string)))
-    });
+    c.bench_function("LeoAst::new", |b| b.iter(|| leo_ast(&filepath, program_string)));
 }
 
 criterion_group!(benches, criterion_benchmark);
