078827310c845d5066770dbdcd62d3f99851d0b9
collin <collin.chin@berkeley.edu>
2020-10-27 15:08:22 -0700

    fix type variable tuple parsing

--- dynamic-check/src/assertions/type_variable_pair.rs
+++ dynamic-check/src/assertions/type_variable_pair.rs
@@ -90,7 +90,9 @@ impl TypeVariablePairs {
             (Type::Array(left_type, left_dimensions), Type::Array(right_type, right_dimensions)) => {
                 self.push_pairs_array(*left_type, left_dimensions, *right_type, right_dimensions, span)
             }
-            (Type::Tuple(left_types), Type::Tuple(right_types)) => self.push_pairs_tuple(left_types, right_types, span),
+            (Type::Tuple(left_types), Type::Tuple(right_types)) => {
+                self.push_pairs_tuple(left_types.into_iter(), right_types.into_iter(), span)
+            }
             (_, _) => Ok(()), // No `TypeVariable` found so we do not push any pairs.
         }
     }
