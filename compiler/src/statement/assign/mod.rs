6bb343728066fa82033884e3f724d057c582fbbe
Protryon <max.bruce12@gmail.com>
2020-12-12 04:49:05 -0800

    remove unused assign code

--- compiler/src/statement/assign/mod.rs
+++ compiler/src/statement/assign/mod.rs
@@ -16,17 +16,8 @@
 
 //! Methods to enforce constraints on assign statements in a compiled Leo program.
 
-pub mod array;
-pub use self::array::*;
-
 pub mod assign;
 pub use self::assign::*;
 
 pub mod assignee;
 pub use self::assignee::*;
-
-pub mod circuit_variable;
-pub use self::circuit_variable::*;
-
-pub mod tuple;
-pub use self::tuple::*;
