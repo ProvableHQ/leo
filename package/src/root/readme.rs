bf2280e400a788917d5687cd61e7a95d58c783b2
howardwu <howardwu@berkeley.edu>
2021-01-03 18:09:14 -0400

    Add bash highlighting

--- package/src/root/readme.rs
+++ package/src/root/readme.rs
@@ -64,19 +64,19 @@ impl README {
 ## Build Guide
 
 To compile this Leo program, run:
-```
+```bash
 leo build
 ```
 
 To test this Leo program, run:
-```
+```bash
 leo test
 ```
 
 ## Development
 
 To output the number of constraints, run:
-```
+```bash
 leo build -d
 ```
 ",
