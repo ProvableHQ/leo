7e7db4d93be8fa118200cf9060f7b958e555c499
gluax <16431709+gluax@users.noreply.github.com>
2021-02-16 15:12:20 -0500

    remove comment
    Co-authored-by: Collin Chin <collin.chin@berkeley.edu>

--- compiler/src/value/field/field_type.rs
+++ compiler/src/value/field/field_type.rs
@@ -40,7 +40,6 @@ use snarkvm_models::{
 use std::{borrow::Borrow, cmp::Ordering};
 
 #[derive(Clone, Debug)]
-// PrimeField already implements Field.
 pub enum FieldType<F: Field + PrimeField> {
     Constant(F),
     Allocated(FpGadget<F>),
