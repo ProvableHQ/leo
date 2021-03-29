8eba9fb21d446d98cd813e1a0c86b575fbfc596e
damirka <damirka.ru@gmail.com>
2021-03-29 18:44:46 +0300

    added string search instead of line count

--- .circleci/leo-check-constraints.sh
+++ .circleci/leo-check-constraints.sh
@@ -6,12 +6,11 @@ export PEDERSEN_HASH_CONSTRAINTS=1539
 
 # 1. build 
 # 2. find lines with constraint number
-# 3. find lines with $PEDERSEN_HASH_CONSTRAINTS
-# 4. count lines
-# 4.Er if result is 0 -> constraint number changed
+# 3. search this line for CONSTRAINTS number
+# 4.Er if result is 0 -> constraint number changed exit 1
 # 4.Ok if result is 1 -> all good
 
-[[ $($LEO build | grep "Number of constraints" | grep $PEDERSEN_HASH_CONSTRAINTS | wc -l) -eq 1 ]] || { 
+[[ $(cargo run -q -- build | grep constraints) == *"$PEDERSEN_HASH_CONSTRAINTS"* ]] || { 
     echo >&2 "Number of constraints for Pedersen Hash is not $PEDERSEN_HASH_CONSTRAINTS"; 
     exit 1; 
-} 
+}
