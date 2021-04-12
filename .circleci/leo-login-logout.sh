9104ed4f912cf2ab783b518f9997f564757b194c
damirka <damirka.ru@gmail.com>
2021-04-12 17:12:07 +0300

    debug .ci

--- .circleci/leo-login-logout.sh
+++ .circleci/leo-login-logout.sh
@@ -3,6 +3,9 @@
 $LEO login -u "$ALEO_PM_USERNAME" -p "$ALEO_PM_PASSWORD"
 $LEO new my-app && cd my-app || exit 1
 
+cat Leo.toml
+which wc
+
 # verify that in Leo.toml there's a line with $ALEO_PM_USERNAME;
 # because at the time of calling `leo new` user is logged in and we're expecting substitution
 [[ $(cat Leo.toml | grep "\[$ALEO_PM_USERNAME\]" | wc -l) -eq 1 ]] || exit 1
