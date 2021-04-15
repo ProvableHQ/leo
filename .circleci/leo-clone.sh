540219e10628c57dd55145474a4a4e6197a36adf
collin <collin.chin@berkeley.edu>
2021-04-15 11:29:27 -0700

    comment out leo-clone ci check yaml

--- .circleci/leo-clone.sh
+++ .circleci/leo-clone.sh
@@ -1,18 +1,18 @@
-## leo clone
-#
-## Clone the test-app package.
-#export PACKAGE="$ALEO_PM_USERNAME/test-app"
-#$LEO clone $PACKAGE
-#
-## Assert that the 'test-app' folder is not empty
-#
-#cd test-app || exit 1
-#if [ "$(ls -A $DIR)" ]; then
-#  echo "$DIR is not empty"
-#else
-#  echo "$DIR is empty"
-#  exit 1
-#fi
-#
-#ls -la
-#$LEO run
+# leo clone
+
+# Clone the test-app package.
+export PACKAGE="$ALEO_PM_USERNAME/test-app"
+$LEO clone $PACKAGE
+
+# Assert that the 'test-app' folder is not empty
+
+cd test-app || exit 1
+if [ "$(ls -A $DIR)" ]; then
+  echo "$DIR is not empty"
+else
+  echo "$DIR is empty"
+  exit 1
+fi
+
+ls -la
+$LEO run
