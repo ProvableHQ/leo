e6b7f0fce3ffd7e5f2250019a24e0f6b089a6e00
Protryon <max.bruce12@gmail.com>
2021-06-04 07:35:50 -0700

    bounds check

--- compiler/src/statement/assign/assignee/array_index.rs
+++ compiler/src/statement/assign/assignee/array_index.rs
@@ -25,7 +25,7 @@ use crate::{
     GroupType,
     Integer,
 };
-use leo_asg::{ConstInt, Expression};
+use leo_asg::{ConstInt, Expression, Node};
 
 use snarkvm_fields::PrimeField;
 use snarkvm_gadgets::utilities::{eq::EvaluateEqGadget, select::CondSelectGadget};
@@ -65,25 +65,34 @@ impl<'a, F: PrimeField, G: GroupType<F>> ConstrainedProgram<'a, F, G> {
                 }
             }
         } else {
+            let span = index.span().cloned().unwrap_or_default();
+            {
+                let array_len: u32 = input
+                    .len()
+                    .try_into()
+                    .map_err(|_| ExpressionError::array_length_out_of_bounds(&span))?;
+                self.array_bounds_check(cs, &&index_resolved, array_len, &span)?;
+            }
+
             for (i, item) in input.iter_mut().enumerate() {
                 let namespace_string = format!(
                     "evaluate dyn array assignment eq {} {}:{}",
-                    i, context.span.line_start, context.span.col_start
+                    i, span.line_start, span.col_start
                 );
                 let eq_namespace = cs.ns(|| namespace_string);
 
                 let index_bounded = i
                     .try_into()
-                    .map_err(|_| ExpressionError::array_index_out_of_legal_bounds(&context.span))?;
+                    .map_err(|_| ExpressionError::array_index_out_of_legal_bounds(&span))?;
                 let const_index = ConstInt::U32(index_bounded).cast_to(&index_resolved.get_type());
                 let index_comparison = index_resolved
                     .evaluate_equal(eq_namespace, &Integer::new(&const_index))
-                    .map_err(|_| ExpressionError::cannot_evaluate("==".to_string(), &context.span))?;
+                    .map_err(|_| ExpressionError::cannot_evaluate("==".to_string(), &span))?;
 
                 let unique_namespace = cs.ns(|| {
                     format!(
                         "select array dyn assignment {} {}:{}",
-                        i, context.span.line_start, context.span.col_start
+                        i, span.line_start, span.col_start
                     )
                 });
                 let value = ConstrainedValue::conditionally_select(
@@ -92,7 +101,7 @@ impl<'a, F: PrimeField, G: GroupType<F>> ConstrainedProgram<'a, F, G> {
                     &context.target_value,
                     &item,
                 )
-                .map_err(|e| ExpressionError::cannot_enforce("conditional select".to_string(), e, &context.span))?;
+                .map_err(|e| ExpressionError::cannot_enforce("conditional select".to_string(), e, &span))?;
                 *item = value;
             }
             Ok(())
