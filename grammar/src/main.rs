1b5f2728e59829a7af6f62162598b4eb85318271
collin <16715212+collinc97@users.noreply.github.com>
2021-06-08 13:15:26 -0700

    deprecate abnf, regenerate tests

--- grammar/src/main.rs
+++ grammar/src/main.rs
@@ -38,180 +38,180 @@
 // ;;;;;;;;;
 // ```
 //
-use abnf::types::{Node, Rule};
-use anyhow::Result;
-use std::collections::{HashMap, HashSet};
-
-/// Processor's scope. Used when code block or definition starts or ends.
-#[derive(Debug, Clone)]
-enum Scope {
-    Free,
-    Code,
-    Definition(Rule),
-}
-
-/// Transforms abnf file into Markdown.
-#[derive(Debug, Clone)]
-struct Processor<'a> {
-    rules: HashMap<String, Rule>,
-    grammar: &'a str,
-    scope: Scope,
-    line: u32,
-    out: String,
-}
-
-impl<'a> Processor<'a> {
-    fn new(grammar: &'a str, abnf: Vec<Rule>) -> Processor<'a> {
-        // we need a hashmap to pull rules easily
-        let rules: HashMap<String, Rule> = abnf.into_iter().map(|rule| (rule.name().to_string(), rule)).collect();
-
-        Processor {
-            grammar,
-            line: 0,
-            out: String::new(),
-            rules,
-            scope: Scope::Free,
-        }
-    }
-
-    /// Main function for this struct.
-    /// Goes through each line and transforms it into proper markdown.
-    fn process(&mut self) {
-        let lines = self.grammar.lines();
-        let mut prev = "";
-
-        for line in lines {
-            self.line += 1;
-
-            // code block in comment (not highlighted as abnf)
-            if let Some(code) = line.strip_prefix(";  ") {
-                self.enter_scope(Scope::Code);
-                self.append_str(code);
-
-            // just comment. end of code block
-            } else if let Some(code) = line.strip_prefix("; ") {
-                self.enter_scope(Scope::Free);
-                self.append_str(code);
-
-            // horizontal rule - section separator
-            } else if line.starts_with(";;;;;;;;;;") {
-                self.enter_scope(Scope::Free);
-                self.append_str("\n--------\n");
-
-            // empty line in comment. end of code block
-            } else if line.starts_with(';') {
-                self.enter_scope(Scope::Free);
-                self.append_str("\n\n");
-
-            // just empty line. end of doc, start of definition
-            } else if line.is_empty() {
-                self.enter_scope(Scope::Free);
-                self.append_str("");
-
-            // definition (may be multiline)
-            } else {
-                // if there's an equality sign and previous line was empty
-                if line.contains('=') && prev.is_empty() {
-                    let (def, _) = line.split_at(line.find('=').unwrap());
-                    let def = def.trim();
-
-                    // try to find rule matching definition or fail
-                    let rule = self.rules.get(&def.to_string()).cloned().unwrap();
-
-                    self.enter_scope(Scope::Definition(rule));
-                }
-
-                self.append_str(line);
-            }
-
-            prev = line;
-        }
-    }
-
-    /// Append new line into output, add newline character.
-    fn append_str(&mut self, line: &str) {
-        self.out.push_str(line);
-        self.out.push('\n');
-    }
-
-    /// Enter new scope (definition or code block). Allows customizing
-    /// pre and post lines for each scope entered or exited.
-    fn enter_scope(&mut self, new_scope: Scope) {
-        match (&self.scope, &new_scope) {
-            // exchange scopes between Free and Code
-            (Scope::Free, Scope::Code) => self.append_str("```"),
-            (Scope::Code, Scope::Free) => self.append_str("```"),
-            // exchange scopes between Free and Definition
-            (Scope::Free, Scope::Definition(rule)) => {
-                self.append_str(&format!("<a name=\"{}\"></a>", rule.name()));
-                self.append_str("```abnf");
-            }
-            (Scope::Definition(rule), Scope::Free) => {
-                let mut rules: Vec<String> = Vec::new();
-                parse_abnf_node(rule.node(), &mut rules);
-
-                // 1. leave only unique keys
-                // 2. map each rule into a link
-                // 3. join results as a list
-                // Note: GitHub only allows custom tags with 'user-content-' prefix
-                let keys = rules
-                    .into_iter()
-                    .collect::<HashSet<_>>()
-                    .into_iter()
-                    .map(|tag| format!("[{}](#user-content-{})", &tag, tag))
-                    .collect::<Vec<String>>()
-                    .join(", ");
-
-                self.append_str("```");
-
-                if !keys.is_empty() {
-                    self.append_str(&format!("\nGo to: _{}_;\n", keys));
-                }
-            }
-            (_, _) => (),
-        };
-
-        self.scope = new_scope;
-    }
-}
-
-/// Recursively parse ABNF Node and fill sum vec with found rule names.
-fn parse_abnf_node(node: &Node, sum: &mut Vec<String>) {
-    match node {
-        // these two are just vectors of rules
-        Node::Alternatives(vec) | Node::Concatenation(vec) => {
-            for node in vec {
-                parse_abnf_node(node, sum);
-            }
-        }
-        Node::Group(node) | Node::Optional(node) => parse_abnf_node(node.as_ref(), sum),
-
-        // push rulename if it is known
-        Node::Rulename(name) => sum.push(name.clone()),
-
-        // do nothing for other nodes
-        _ => (),
-    }
-}
-
-fn main() -> Result<()> {
-    // Take Leo ABNF grammar file.
-    let grammar = include_str!("../abnf-grammar.txt");
-
-    // Parse ABNF to get list of all definitions.
-    // Rust ABNF does not provide support for `%s` (case sensitive strings, part of
-    // the standard); so we need to remove all occurrences before parsing.
-    let parsed = abnf::rulelist(&str::replace(grammar, "%s", "")).map_err(|e| {
-        eprintln!("{}", &e);
-        anyhow::anyhow!(e)
-    })?;
-
-    // Init parser and run it. That's it.
-    let mut parser = Processor::new(grammar, parsed);
-    parser.process();
-
-    // Print result of conversion to STDOUT.
-    println!("{}", parser.out);
-
-    Ok(())
-}
+// use abnf::types::{Node, Rule};
+// use anyhow::Result;
+// use std::collections::{HashMap, HashSet};
+//
+// /// Processor's scope. Used when code block or definition starts or ends.
+// #[derive(Debug, Clone)]
+// enum Scope {
+//     Free,
+//     Code,
+//     Definition(Rule),
+// }
+//
+// /// Transforms abnf file into Markdown.
+// #[derive(Debug, Clone)]
+// struct Processor<'a> {
+//     rules: HashMap<String, Rule>,
+//     grammar: &'a str,
+//     scope: Scope,
+//     line: u32,
+//     out: String,
+// }
+//
+// impl<'a> Processor<'a> {
+//     fn new(grammar: &'a str, abnf: Vec<Rule>) -> Processor<'a> {
+//         // we need a hashmap to pull rules easily
+//         let rules: HashMap<String, Rule> = abnf.into_iter().map(|rule| (rule.name().to_string(), rule)).collect();
+//
+//         Processor {
+//             grammar,
+//             line: 0,
+//             out: String::new(),
+//             rules,
+//             scope: Scope::Free,
+//         }
+//     }
+//
+//     /// Main function for this struct.
+//     /// Goes through each line and transforms it into proper markdown.
+//     fn process(&mut self) {
+//         let lines = self.grammar.lines();
+//         let mut prev = "";
+//
+//         for line in lines {
+//             self.line += 1;
+//
+//             // code block in comment (not highlighted as abnf)
+//             if let Some(code) = line.strip_prefix(";  ") {
+//                 self.enter_scope(Scope::Code);
+//                 self.append_str(code);
+//
+//             // just comment. end of code block
+//             } else if let Some(code) = line.strip_prefix("; ") {
+//                 self.enter_scope(Scope::Free);
+//                 self.append_str(code);
+//
+//             // horizontal rule - section separator
+//             } else if line.starts_with(";;;;;;;;;;") {
+//                 self.enter_scope(Scope::Free);
+//                 self.append_str("\n--------\n");
+//
+//             // empty line in comment. end of code block
+//             } else if line.starts_with(';') {
+//                 self.enter_scope(Scope::Free);
+//                 self.append_str("\n\n");
+//
+//             // just empty line. end of doc, start of definition
+//             } else if line.is_empty() {
+//                 self.enter_scope(Scope::Free);
+//                 self.append_str("");
+//
+//             // definition (may be multiline)
+//             } else {
+//                 // if there's an equality sign and previous line was empty
+//                 if line.contains('=') && prev.is_empty() {
+//                     let (def, _) = line.split_at(line.find('=').unwrap());
+//                     let def = def.trim();
+//
+//                     // try to find rule matching definition or fail
+//                     let rule = self.rules.get(&def.to_string()).cloned().unwrap();
+//
+//                     self.enter_scope(Scope::Definition(rule));
+//                 }
+//
+//                 self.append_str(line);
+//             }
+//
+//             prev = line;
+//         }
+//     }
+//
+//     /// Append new line into output, add newline character.
+//     fn append_str(&mut self, line: &str) {
+//         self.out.push_str(line);
+//         self.out.push('\n');
+//     }
+//
+//     /// Enter new scope (definition or code block). Allows customizing
+//     /// pre and post lines for each scope entered or exited.
+//     fn enter_scope(&mut self, new_scope: Scope) {
+//         match (&self.scope, &new_scope) {
+//             // exchange scopes between Free and Code
+//             (Scope::Free, Scope::Code) => self.append_str("```"),
+//             (Scope::Code, Scope::Free) => self.append_str("```"),
+//             // exchange scopes between Free and Definition
+//             (Scope::Free, Scope::Definition(rule)) => {
+//                 self.append_str(&format!("<a name=\"{}\"></a>", rule.name()));
+//                 self.append_str("```abnf");
+//             }
+//             (Scope::Definition(rule), Scope::Free) => {
+//                 let mut rules: Vec<String> = Vec::new();
+//                 parse_abnf_node(rule.node(), &mut rules);
+//
+//                 // 1. leave only unique keys
+//                 // 2. map each rule into a link
+//                 // 3. join results as a list
+//                 // Note: GitHub only allows custom tags with 'user-content-' prefix
+//                 let keys = rules
+//                     .into_iter()
+//                     .collect::<HashSet<_>>()
+//                     .into_iter()
+//                     .map(|tag| format!("[{}](#user-content-{})", &tag, tag))
+//                     .collect::<Vec<String>>()
+//                     .join(", ");
+//
+//                 self.append_str("```");
+//
+//                 if !keys.is_empty() {
+//                     self.append_str(&format!("\nGo to: _{}_;\n", keys));
+//                 }
+//             }
+//             (_, _) => (),
+//         };
+//
+//         self.scope = new_scope;
+//     }
+// }
+//
+// /// Recursively parse ABNF Node and fill sum vec with found rule names.
+// fn parse_abnf_node(node: &Node, sum: &mut Vec<String>) {
+//     match node {
+//         // these two are just vectors of rules
+//         Node::Alternatives(vec) | Node::Concatenation(vec) => {
+//             for node in vec {
+//                 parse_abnf_node(node, sum);
+//             }
+//         }
+//         Node::Group(node) | Node::Optional(node) => parse_abnf_node(node.as_ref(), sum),
+//
+//         // push rulename if it is known
+//         Node::Rulename(name) => sum.push(name.clone()),
+//
+//         // do nothing for other nodes
+//         _ => (),
+//     }
+// }
+//
+// fn main() -> Result<()> {
+//     // Take Leo ABNF grammar file.
+//     let grammar = include_str!("../abnf-grammar.txt");
+//
+//     // Parse ABNF to get list of all definitions.
+//     // Rust ABNF does not provide support for `%s` (case sensitive strings, part of
+//     // the standard); so we need to remove all occurrences before parsing.
+//     let parsed = abnf::rulelist(&str::replace(grammar, "%s", "")).map_err(|e| {
+//         eprintln!("{}", &e);
+//         anyhow::anyhow!(e)
+//     })?;
+//
+//     // Init parser and run it. That's it.
+//     let mut parser = Processor::new(grammar, parsed);
+//     parser.process();
+//
+//     // Print result of conversion to STDOUT.
+//     println!("{}", parser.out);
+//
+//     Ok(())
+// }
