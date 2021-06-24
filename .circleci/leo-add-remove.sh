7205fbe22afa5671021dd11120c30d593d43bb1f
damirka <damirka.ru@gmail.com>
2021-06-24 20:13:18 +0300

    fixed env variable

--- .circleci/leo-add-remove.sh
+++ .circleci/leo-add-remove.sh
@@ -1,8 +1,8 @@
 # leo add (w/o login) & remove
 
-LEO new my-app && cd my-app || exit 1
-LEO add justice-league/u8u32
+$LEO new my-app && cd my-app || exit 1
+$LEO add justice-league/u8u32
 echo "import u8u32.u8_u32; function main() {}" > src/main.leo
-LEO build
-LEO remove u8u32
-LEO clean
+$LEO build
+$LEO remove u8u32
+$LEO clean
