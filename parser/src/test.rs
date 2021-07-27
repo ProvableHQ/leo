5cf185369d60712db9ea45147ea66e797e10e688
gluaxspeed <jonathan.t.pavlik@gmail.com>
2021-07-27 12:11:31 -0700

    clippy fix/cleanup

--- parser/src/test.rs
+++ parser/src/test.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
-use colored::Colorize;
 use leo_ast::{Expression, ExpressionStatement, Span, Statement, ValueExpression};
 use leo_test_framework::{
     runner::{Namespace, ParseType, Runner},
