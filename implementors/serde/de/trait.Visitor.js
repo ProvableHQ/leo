f5b08aaf4dcd4f3f5884f060448c28df694206d0
bendyarm <bendyarm@users.noreply.github.com>
2021-09-30 22:36:07 +0000

    deploy: e2ba64b79d51eb8c0fcdc413d9218f363e91c821

---
+++ implementors/serde/de/trait.Visitor.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["leo_errors"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.130/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for <a class=\"struct\" href=\"leo_errors/common/span_json/struct.SpanMapVisitor.html\" title=\"struct leo_errors::common::span_json::SpanMapVisitor\">SpanMapVisitor</a>","synthetic":false,"types":["leo_errors::common::span_json::SpanMapVisitor"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- tgc/sidebar-items.js
+++ tgc/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["generate_asts","Do what Compiler does - prepare 3 stages of AST: initial, canonicalized and type_inferenced"],["handle_error",""],["main",""],["run_with_args",""]],"struct":[["Opt",""]]});
+initSidebarItems({"fn":[["generate_asts","Do what Compiler does - prepare 3 stages of AST: initial, imports_resolved, canonicalized and type_inferenced Write these ASTs without Spans to JSON"],["handle_error",""],["main",""],["run_with_args",""]],"struct":[["Opt",""]]});
