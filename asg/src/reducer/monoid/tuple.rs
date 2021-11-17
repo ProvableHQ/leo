527bffa808b5b0daa2cc04bcca4f54d4e8999ccd
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-11-17 11:51:35 -0800

    Cleaned up tuple monoid

--- asg/src/reducer/monoid/tuple.rs
+++ asg/src/reducer/monoid/tuple.rs
@@ -15,147 +15,129 @@
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 use super::*;
 
-macro_rules! tuple_append {
-    ($id1: ident, $id2:ident, (((((((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        ($id1.0.append($id2.0),)
-    };
-    ($id1: ident, $id2:ident, ((((((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        ($id1.0.append($id2.0), $id1.1.append($id2.1))
-    };
-    ($id1:ident, $id2:ident, (((((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        ($id1.0.append($id2.0), $id1.1.append($id2.1), $id1.2.append($id2.2))
-    };
-    ($id1:ident, $id2:ident, ((((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-        )
-    };
-    ($id1:ident, $id2:ident, (((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-        )
-    };
-    ($id1:ident, $id2:ident, ((((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-        )
-    };
-    ($id1:ident, $id2: ident, (((((12u32 - 1u32) - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-        )
-    };
-    ($id1:ident, $id2: ident, ((((12u32 - 1u32) - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-            $id1.7.append($id2.7),
-        )
-    };
-    ($id1:ident, $id2:ident, (((12u32 - 1u32) - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-            $id1.7.append($id2.7),
-            $id1.8.append($id2.8),
-        )
-    };
-    ($id1:ident, $id2:ident, ((12u32 - 1u32) - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-            $id1.7.append($id2.7),
-            $id1.8.append($id2.8),
-            $id1.9.append($id2.9),
-        )
-    };
-    ($id1:ident, $id2:ident, (12u32 - 1u32)) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-            $id1.7.append($id2.7),
-            $id1.8.append($id2.8),
-            $id1.9.append($id2.9),
-            $id1.10.append($id2.10),
-        )
-    };
-    ($id1:ident, $id2: ident, 12u32) => {
-        (
-            $id1.0.append($id2.0),
-            $id1.1.append($id2.1),
-            $id1.2.append($id2.2),
-            $id1.3.append($id2.3),
-            $id1.4.append($id2.4),
-            $id1.5.append($id2.5),
-            $id1.6.append($id2.6),
-            $id1.7.append($id2.7),
-            $id1.8.append($id2.8),
-            $id1.9.append($id2.9),
-            $id1.10.append($id2.10),
-            $id1.11.append($id2.11),
-        )
-    };
-}
-
+// macro for implementing n-ary tuple functions and operations
 macro_rules! tuple_monoid_impls {
-    (@count $i:tt, $head:ident, $( $tail:ident, )* ) => {
-        impl<$head, $( $tail ),*> Monoid for ($head, $( $tail ),*)
-        where
-            $head: Monoid,
-            $( $tail: Monoid),*
-        {
-
-            fn append(self, other: ($head, $( $tail ),*)) -> ($head, $( $tail ),*) {
-
-                tuple_append!(self, other, $i)
-            }
-
-            fn append_all(self, others: impl Iterator<Item = ($head, $( $tail),* )>) -> ($head, $( $tail ),*) {
-                others.fold(self, |acc, tup| acc.append(tup))
-            }
-
+    ($(
+        $Tuple:ident {
+            $(($idx:tt) -> $T:ident)+
         }
-        tuple_monoid_impls!(@count ($i - 1u32), $( $tail, )*);
-    };
-    (@count $i:tt,) => {};
+    )+) => {
+        $(
+            impl<$($T:Monoid),+> Monoid for ($($T,)+) {
+
+                fn append(self, other: ($($T,)+)) -> ($($T,)+) {
+                    ($(self.$idx.append(other.$idx)),+,)
+                }
+
+                fn append_all(self, others: impl Iterator<Item = ($($T,)+)>) -> ($($T,)+) {
+                    others.fold(self, |acc, tup| acc.append(tup))
+                }
+            }
+        )+
+    }
 }
 
-tuple_monoid_impls!(@count 12u32, A, B, C, D, E, F, G, H, I, J, K, L,);
+tuple_monoid_impls! {
+    Tuple1 {
+        (0) -> A
+    }
+    Tuple2 {
+        (0) -> A
+        (1) -> B
+    }
+    Tuple3 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+    }
+    Tuple4 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+    }
+    Tuple5 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+    }
+    Tuple6 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+    }
+    Tuple7 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+    }
+    Tuple8 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+    }
+    Tuple9 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+    }
+    Tuple10 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+    }
+    Tuple11 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+    }
+    Tuple12 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+        (11) -> L
+    }
+}
