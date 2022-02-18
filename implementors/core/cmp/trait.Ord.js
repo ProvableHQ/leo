793a94e724825707e128f67cc6fc2d6929eca24f
collinc97 <collinc97@users.noreply.github.com>
2022-02-18 19:18:42 +0000

    deploy: 045fef787f7aa4519b1f37721eb2bbd96387a861

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["leo_input"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"leo_input/ast/enum.Rule.html\" title=\"enum leo_input::ast::Rule\">Rule</a>","synthetic":false,"types":["leo_input::ast::Rule"]}];
+implementors["leo_input"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"leo_input/ast/enum.Rule.html\" title=\"enum leo_input::ast::Rule\">Rule</a>","synthetic":false,"types":["leo_input::ast::Rule"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
