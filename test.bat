ba6f2359371e76663e37624224d165387ae4d5bb
0rphon <59403052+0rphon@users.noreply.github.com>
2022-02-23 15:07:32 -0800

    added some more test cases

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
+@REM cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture cargo 2>&1 | rp -B 2 dbg 
 set CLEAR_LEO_TEST_EXPECTATIONS=1
 cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture
 set CLEAR_LEO_TEST_EXPECTATIONS=
