0e1d1a43e9fbce99d6824fae49a8cff54c2a6abb
0rphon <59403052+0rphon@users.noreply.github.com>
2022-03-15 15:38:06 -0700

    removed tools/failing tests

--- test.bat
+++
@@ -1,4 +0,0 @@
-@REM cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture cargo 2>&1 | rg -B 2 dbg 
-set CLEAR_LEO_TEST_EXPECTATIONS=1
-cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture
-set CLEAR_LEO_TEST_EXPECTATIONS=
