d260ee873b719f3f720a297148d266f33b73cab0
0rphon <59403052+0rphon@users.noreply.github.com>
2022-03-24 21:13:00 -0700

    removed scripts

--- test.sh
+++
@@ -1,4 +0,0 @@
-export CLEAR_LEO_TEST_EXPECTATIONS=1
-cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture
-# cargo test --package leo-parser --lib -- test::parser_tests --exact --nocapture 2>&1 | grep -A 2 dbg
-export CLEAR_LEO_TEST_EXPECTATIONS=
