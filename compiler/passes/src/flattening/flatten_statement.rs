fdee58ba4d66213506fe0bebdec4bad6b3fbb809
gluax <16431709+gluax@users.noreply.github.com>
2022-06-27 12:59:29 -0700

    never entered original iteration scope

--- compiler/passes/src/flattening/flatten_statement.rs
+++ compiler/passes/src/flattening/flatten_statement.rs
@@ -224,6 +224,13 @@ impl<'a> StatementReconstructor for Flattener<'a> {
                     Default::default()
                 };
 
+                let scope_index = self.block_index;
+                let prev_st = std::mem::take(&mut self.symbol_table);
+                self.symbol_table
+                    .swap(prev_st.borrow().get_block_scope(scope_index).unwrap());
+                self.symbol_table.borrow_mut().parent = Some(Box::new(prev_st.into_inner()));
+                self.block_index = 0;
+
                 let iter_blocks = Statement::Block(Block {
                     statements: range
                         .into_iter()
@@ -271,7 +278,11 @@ impl<'a> StatementReconstructor for Flattener<'a> {
                         .collect(),
                     span: input.span,
                 });
-                self.block_index += 1;
+                let prev_st = *self.symbol_table.borrow_mut().parent.take().unwrap();
+                self.symbol_table.swap(prev_st.get_block_scope(scope_index).unwrap());
+                self.symbol_table = RefCell::new(prev_st);
+                self.block_index = scope_index + 1;
+
                 return iter_blocks;
             }
             (None, Some(_)) => self
