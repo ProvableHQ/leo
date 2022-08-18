3af5c132c6630191956b85ca5b052a37a977436d
d0cd <d0cd@users.noreply.github.com>
2022-08-18 23:32:12 +0000

    deploy: 4c0aed8718bea1ce79b6c57db69821fac139d27b

---
+++ implementors/leo_ast/passes/consumer/trait.FunctionConsumer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["leo_passes"] = [{"text":"impl <a class=\"trait\" href=\"leo_ast/passes/consumer/trait.FunctionConsumer.html\" title=\"trait leo_ast::passes::consumer::FunctionConsumer\">FunctionConsumer</a> for <a class=\"struct\" href=\"leo_passes/static_single_assignment/static_single_assigner/struct.StaticSingleAssigner.html\" title=\"struct leo_passes::static_single_assignment::static_single_assigner::StaticSingleAssigner\">StaticSingleAssigner</a>&lt;'_&gt;","synthetic":false,"types":["leo_passes::static_single_assignment::static_single_assigner::StaticSingleAssigner"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
