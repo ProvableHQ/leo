8b83e35c4eb2643e8dab8a9e65f625804f6994d0
collin <16715212+collinc97@users.noreply.github.com>
2022-09-19 18:29:00 -0700

    commit bash script

--- examples/vote/run.sh
+++ examples/vote/run.sh
@@ -7,3 +7,15 @@
 (
  leo run propose || exit
 )
+
+# Run the `new_ticket` program function
+(
+  leo run new_ticket || exit
+)
+
+# Run the `agree` or `disagree` program function
+(
+  leo run agree || exit
+  # leo run disagree || exit
+)
+
