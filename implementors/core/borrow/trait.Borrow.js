c829bf91863374a60d543d7b91ce6be7b3640d06
d0cd <d0cd@users.noreply.github.com>
2023-01-18 18:29:02 +0000

    deploy: 304e3228d7e06ad14e036245a2f58340fa735b3f

--- implementors/core/borrow/trait.Borrow.js
+++ implementors/core/borrow/trait.Borrow.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"leo_span":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"leo_span/symbol/enum.InternedStr.html\" title=\"enum leo_span::symbol::InternedStr\">InternedStr</a>"]]
+"leo_span":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"leo_span/symbol/enum.InternedStr.html\" title=\"enum leo_span::symbol::InternedStr\">InternedStr</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
