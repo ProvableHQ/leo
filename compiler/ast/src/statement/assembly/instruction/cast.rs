c17c193553a4e1c6859de4b633c273e7534a9485
d0cd <pranavsaig@gmail.com>
2023-03-14 09:56:20 -0700

    Add Cast instruction to AST

---
+++ compiler/ast/src/statement/assembly/instruction/cast.rs
@@ -0,0 +1,43 @@
+// Copyright (C) 2019-2023 Aleo Systems Inc.
+// This file is part of the Leo library.
+
+// The Leo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The Leo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{Identifier, Node, Operand};
+
+use leo_span::Span;
+
+use core::fmt;
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
+pub struct Cast {
+    pub arguments: Vec<Operand>,
+    pub destination: Identifier,
+    pub span: Span,
+}
+
+impl fmt::Display for Cast {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "cast {} into {};",
+            self.arguments.iter().map(|arg| arg.to_string()).join(" "),
+            self.destination
+        )
+    }
+}
+
+crate::simple_node_impl!(Cast);
