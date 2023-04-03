3b20befc97a81821e04b660948e688bb5743f954
Pranav Gaddamadugu <pranav@aleo.org>
2023-04-03 10:04:34 -0700

    Fix deprecation warning for increment/decrement statements

--- errors/src/errors/parser/parser_warnings.rs
+++ errors/src/errors/parser/parser_warnings.rs
@@ -16,6 +16,8 @@
 
 use crate::create_messages;
 
+use std::fmt::Display;
+
 create_messages!(
     /// ParserWarning enum that represents all the warnings for the `leo-parser` crate.
     ParserWarning,
@@ -23,10 +25,21 @@ create_messages!(
     code_prefix: "PAR",
 
     /// For when a user used const on a parameter or input instead of constant.
-     @formatted
-     const_parameter_or_input {
-         args: (),
-         msg: "`constant` is preferred over `const` for function parameters to indicate a R1CS constant.",
-         help: None,
-     }
+    @formatted
+    const_parameter_or_input {
+        args: (),
+        msg: "`constant` is preferred over `const` for function parameters to indicate a R1CS constant.",
+        help: None,
+    }
+
+    /// For when a keyword is deprecated but could be used as a valid identifier.
+    @formatted
+    deprecated {
+        args: (keyword: impl Display, help: impl Display),
+        msg: format!("The keyword `{keyword}` is deprecated."),
+        help: Some(help.to_string()),
+    }
+
+
+
 );
