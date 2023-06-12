194fe3ac150b1918a2b4167cb65b1f9d8115999b
collin <16715212+collinc97@users.noreply.github.com>
2023-06-12 15:34:41 -0700

    fix codegen

--- compiler/passes/src/code_generation/visit_expressions.rs
+++ compiler/passes/src/code_generation/visit_expressions.rs
@@ -312,7 +312,7 @@ impl<'a> CodeGenerator<'a> {
                     (destination_register, instruction)
                 }
                 sym::get_or_use => {
-                    let mut instruction = "    get.or_init".to_string();
+                    let mut instruction = "    get.or_use".to_string();
                     let destination_register = get_destination_register();
                     // Write the mapping name, the key, and the default value.
                     writeln!(
