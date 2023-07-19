826c27e08a56a613e1a204aeaf5e9f6efc883c7b
collin <16715212+collinc97@users.noreply.github.com>
2023-07-19 16:13:43 -0700

    do not commit .avm files

--- leo/package/src/root/gitignore.rs
+++ leo/package/src/root/gitignore.rs
@@ -51,6 +51,6 @@ impl Gitignore {
     }
 
     fn template(&self) -> String {
-        ".env\n*.prover\n*.verifier\noutputs/\n".to_string()
+        ".env\n*.avm\n*.prover\n*.verifier\noutputs/\n".to_string()
     }
 }
