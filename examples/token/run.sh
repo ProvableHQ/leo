3bd51694f3445b7b4ef1ef4e5bd81c37895c07c3
Collin Chin <16715212+collinc97@users.noreply.github.com>
2023-07-21 16:48:53 -0700

    Update examples/token/run.sh
    Co-authored-by: Eric McCarthy <7607035+bendyarm@users.noreply.github.com>
    Signed-off-by: Collin Chin <16715212+collinc97@users.noreply.github.com>

--- examples/token/run.sh
+++ examples/token/run.sh
@@ -11,7 +11,7 @@ fi
 # "private_key": "APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR",
 # "address": "aleo13ssze66adjjkt795z9u5wpq8h6kn0y2657726h4h3e3wfnez4vqsm3008q"
 
-# The private key and address of the Bob.
+# The private key and address of Bob.
 # Swap these into program.json, when running transactions as the second bidder.
 # "private_key": "APrivateKey1zkpFo72g7N9iFt3JzzeG8CqsS5doAiXyFvNCgk2oHvjRCzF"
 # "address": "aleo17vy26rpdhqx4598y5gp7nvaa9rk7tnvl6ufhvvf4calsrrqdaqyshdsf5z"
