4221ab5bbc6e92b9f9a9b53812c68981c1a863ad
collin <16715212+collinc97@users.noreply.github.com>
2023-07-21 17:00:23 -0700

    fix comments

--- examples/token/run.sh
+++ examples/token/run.sh
@@ -6,7 +6,7 @@ then
     exit
 fi
 
-# The private key and address of Alice
+# The private key and address of Alice.
 # Swap these into program.json, when running transactions as the first bidder.
 # "private_key": "APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR",
 # "address": "aleo13ssze66adjjkt795z9u5wpq8h6kn0y2657726h4h3e3wfnez4vqsm3008q"
@@ -16,7 +16,7 @@ fi
 # "private_key": "APrivateKey1zkpFo72g7N9iFt3JzzeG8CqsS5doAiXyFvNCgk2oHvjRCzF"
 # "address": "aleo17vy26rpdhqx4598y5gp7nvaa9rk7tnvl6ufhvvf4calsrrqdaqyshdsf5z"
 
-# Swap in the private key and address of Alice to .env.
+# Swap in the private key of Alice.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR
@@ -50,7 +50,7 @@ echo "
 "
 leo run mint_public aleo13ssze66adjjkt795z9u5wpq8h6kn0y2657726h4h3e3wfnez4vqsm3008q 100u64
 
-# Swap in the private key and address of Bob to .env.
+# Swap in the private key of Bob.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkpFo72g7N9iFt3JzzeG8CqsS5doAiXyFvNCgk2oHvjRCzF
@@ -84,7 +84,7 @@ echo "
 "
 leo run mint_private aleo17vy26rpdhqx4598y5gp7nvaa9rk7tnvl6ufhvvf4calsrrqdaqyshdsf5z 100u64
 
-# Swap in the private key and address of Alice to .env.
+# Swap in the private key of Alice.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR
@@ -118,7 +118,7 @@ echo "
 "
 leo run transfer_public aleo17vy26rpdhqx4598y5gp7nvaa9rk7tnvl6ufhvvf4calsrrqdaqyshdsf5z 10u64
 
-# Swap in the private key and address of Bob to .env.
+# Swap in the private key of Bob.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkpFo72g7N9iFt3JzzeG8CqsS5doAiXyFvNCgk2oHvjRCzF
@@ -156,7 +156,7 @@ leo run transfer_private "{
         _nonce: 6586771265379155927089644749305420610382723873232320906747954786091923851913group.public
     }" aleo13ssze66adjjkt795z9u5wpq8h6kn0y2657726h4h3e3wfnez4vqsm3008q 20u64
 
-# Swap in the private key and address of Alice to .env.
+# Swap in the private key of Alice.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR
@@ -191,7 +191,7 @@ echo "
 "
 leo run transfer_public_to_private aleo17vy26rpdhqx4598y5gp7nvaa9rk7tnvl6ufhvvf4calsrrqdaqyshdsf5z 30u64
 
-# Swap in the private key and address of Bob to .env.
+# Swap in the private key of Bob.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkpFo72g7N9iFt3JzzeG8CqsS5doAiXyFvNCgk2oHvjRCzF
@@ -231,9 +231,8 @@ leo run transfer_private_to_public "{
     }" aleo13ssze66adjjkt795z9u5wpq8h6kn0y2657726h4h3e3wfnez4vqsm3008q 40u64
 
 
-# Swap in the private key and address of the Alice to program.json.
+# Swap in the private key of Alice.
 # This is done to ensure that program.json is the same after every execution of ./run.sh.
-# Swap in the private key and address of the Alice to .env.
 echo "
 NETWORK=testnet3
 PRIVATE_KEY=APrivateKey1zkp1w8PTxrRgGfAtfKUSq43iQyVbdQHfhGbiNPEg2LVSEXR
