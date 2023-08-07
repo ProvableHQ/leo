e62d0a0ab537b5d0fca51ea46ad5dee5334d31df
d0cd <d0cd@users.noreply.github.com>
2023-08-07 16:17:02 +0000

    deploy: 79c51bc33b6c098ce7eb53630e37cd4bc5ad7576

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"leo_ast":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"leo_ast/types/tuple/struct.Tuple.html\" title=\"struct leo_ast::types::tuple::Tuple\">Tuple</a>"]],
-"leo_span":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"enum\" href=\"leo_span/symbol/enum.InternedStr.html\" title=\"enum leo_span::symbol::InternedStr\">InternedStr</a>"]]
+"leo_ast":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"leo_ast/types/tuple/struct.Tuple.html\" title=\"struct leo_ast::types::tuple::Tuple\">Tuple</a>"]],
+"leo_span":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"enum\" href=\"leo_span/symbol/enum.InternedStr.html\" title=\"enum leo_span::symbol::InternedStr\">InternedStr</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
