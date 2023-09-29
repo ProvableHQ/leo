b73bd734bb88305ec6037862b3c7f15aca41c014
Pranav Gaddamadugu <pranav@aleo.org>
2023-09-29 12:25:30 -0400

    Update CI and examples

--- .circleci/test-examples.sh
+++ .circleci/test-examples.sh
@@ -4,255 +4,249 @@ leo() {
   $LEO "$@"
 }
 
-# Build and run the auction Leo program.
-echo "Building and running the \`auction\` program..."
-(
-  cd $EXAMPLES/auction || exit
-  $LEO run place_bid || exit
-  $LEO run resolve || exit
-  $LEO run finish || exit
-
-  chmod +x $EXAMPLES/auction/run.sh || exit
-  export -f leo || exit
-  $EXAMPLES/auction/run.sh || exit
-)
-# Check that the auction program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`auction\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the basic_bank Leo program.
-echo "Building and running the \`basic_bank\` program..."
-(
-  cd $EXAMPLES/basic_bank || exit
-  $LEO run issue || exit
-  $LEO run deposit || exit
-  $LEO run withdraw || exit
-
-  chmod +x $EXAMPLES/basic_bank/run.sh || exit
-  export -f leo || exit
-  $EXAMPLES/basic_bank/run.sh || exit
-)
-# Check that the basic_bank program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`basic_bank\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the battleship Leo program.
-echo "Building and running the \`battleship\` program..."
-which leo
-(
-  cd $EXAMPLES/battleship || exit
-
-  chmod +x $EXAMPLES/battleship/run.sh || exit
-  export -f leo || exit
-  $EXAMPLES/battleship/run.sh || exit
-)
-# Check that the battleship program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`battleship\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the bubblesort Leo program.
-echo "Building and running the \`bubblesort\` program..."
-(
-  cd $EXAMPLES/bubblesort || exit
-  $LEO run bubble_sort || exit
-)
-# Check that the bubblesort program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`bubblesort\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the core example Leo program.
-echo "Building and running the \`core\` program..."
-(
-  cd $EXAMPLES/core || exit
-  $LEO run main || exit
-)
-# Check that the core program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`core\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the groups example Leo program.
-echo "Building and running the \`groups\` program..."
-(
-  cd $EXAMPLES/groups || exit
-  $LEO run main || exit
-)
-# Check that the groups program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`groups\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzdebruijn program.
-echo "Building and running the \`hackers-delight/ntzdebruijn\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzdebruijn || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzdebruijn program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzdebruijn\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzgaudet program.
-echo "Building and running the \`hackers-delight/ntzgaudet\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzgaudet || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzgaudet program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzgaudet\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzloops program.
-echo "Building and running the \`hackers-delight/ntzloops\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzloops || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzloops program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzloops\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzmasks program.
-echo "Building and running the \`hackers-delight/ntzmasks\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzmasks || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzmasks program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzmasks\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzreisers program.
-echo "Building and running the \`hackers-delight/ntzreisers\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzreisers || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzreisers program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzreisers\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzseals program.
-echo "Building and running the \`hackers-delight/ntzseals\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzseals || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzseals program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzseals\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzsearchtree program.
-echo "Building and running the \`hackers-delight/ntzsearchtree\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzsearchtree || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzsearchtree program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzsearchtree\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the hackers-delight/ntzsmallvals program.
-echo "Building and running the \`hackers-delight/ntzsmallvals\` program..."
-(
-  cd $EXAMPLES/hackers-delight/ntzsmallvals || exit
-  $LEO run || exit
-)
-# Check that the hackers-delight/ntzsmallvals program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`hackers-delight/ntzsmallvals\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the helloworld Leo program.
-echo "Building and running the \`helloworld\` program..."
-(
-  cd $EXAMPLES/helloworld || exit
-  $LEO run main || exit
-)
-# Check that the helloworld program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`helloworld\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-
-# Build and run the interest example Leo programs.
-echo "Building and running the \`interest\` programs..."
-(
-  cd $EXAMPLES/interest || exit
-
-  # Run the fixed period interest program.
-  $LEO run fixed_iteration_interest || exit
-
-  # Run the bounded period interest program.
-  $LEO run bounded_iteration_interest || exit
-)
-# Check that the interest programs ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`interest\` program failed to run successfully."
-    exit $EXITCODE
-fi
-
-# Build and run the message example Leo program.
-echo "Building and running the \`message\` program..."
-(
-  cd $EXAMPLES/message || exit
-  $LEO run main || exit
-)
-# Check that the message program ran successfully.
-EXITCODE=$?
-if [ $EXITCODE -ne 0 ]; then
-    echo "The \`message\` program failed to run successfully."
-    exit $EXITCODE
-fi
+## Build and run the auction Leo program.
+#echo "Building and running the \`auction\` program..."
+#(
+#  cd $EXAMPLES/auction || exit
+#
+#  chmod +x $EXAMPLES/auction/run.sh || exit
+#  export -f leo || exit
+#  $EXAMPLES/auction/run.sh || exit
+#)
+## Check that the auction program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`auction\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the basic_bank Leo program.
+#echo "Building and running the \`basic_bank\` program..."
+#(
+#  cd $EXAMPLES/basic_bank || exit
+#
+#  chmod +x $EXAMPLES/basic_bank/run.sh || exit
+#  export -f leo || exit
+#  $EXAMPLES/basic_bank/run.sh || exit
+#)
+## Check that the basic_bank program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`basic_bank\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the battleship Leo program.
+#echo "Building and running the \`battleship\` program..."
+#which leo
+#(
+#  cd $EXAMPLES/battleship || exit
+#
+#  chmod +x $EXAMPLES/battleship/run.sh || exit
+#  export -f leo || exit
+#  $EXAMPLES/battleship/run.sh || exit
+#)
+## Check that the battleship program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`battleship\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the bubblesort Leo program.
+#echo "Building and running the \`bubblesort\` program..."
+#(
+#  cd $EXAMPLES/bubblesort || exit
+#  cat $EXAMPLES/bubblesort/inputs/bubblesort.in | xargs $LEO run bubble_sort || exit
+#)
+## Check that the bubblesort program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`bubblesort\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the core example Leo program.
+#echo "Building and running the \`core\` program..."
+#(
+#  cd $EXAMPLES/core || exit
+#  cat $EXAMPLES/core/inputs/core.in | xargs $LEO run main || exit
+#)
+## Check that the core program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`core\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the groups example Leo program.
+#echo "Building and running the \`groups\` program..."
+#(
+#  cd $EXAMPLES/groups || exit
+#  cat $EXAMPLES/groups/inputs/groups.in | xargs $LEO run main || exit
+#)
+## Check that the groups program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`groups\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzdebruijn program.
+#echo "Building and running the \`hackers-delight/ntzdebruijn\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzdebruijn || exit
+#  cat $EXAMPLES/hackers-delight/ntzdebruijn/inputs/ntzdebruijn.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzdebruijn program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzdebruijn\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzgaudet program.
+#echo "Building and running the \`hackers-delight/ntzgaudet\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzgaudet || exit
+#  cat $EXAMPLES/hackers-delight/ntzgaudet/inputs/ntzgaudet.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzgaudet program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzgaudet\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzloops program.
+#echo "Building and running the \`hackers-delight/ntzloops\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzloops || exit
+#  cat $EXAMPLES/hackers-delight/ntzloops/inputs/ntzloops.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzloops program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzloops\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzmasks program.
+#echo "Building and running the \`hackers-delight/ntzmasks\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzmasks || exit
+#  cat $EXAMPLES/hackers-delight/ntzmasks/inputs/ntzmasks.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzmasks program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzmasks\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzreisers program.
+#echo "Building and running the \`hackers-delight/ntzreisers\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzreisers || exit
+#  cat $EXAMPLES/hackers-delight/ntzreisers/inputs/ntzreisers.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzreisers program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzreisers\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzseals program.
+#echo "Building and running the \`hackers-delight/ntzseals\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzseals || exit
+#  cat $EXAMPLES/hackers-delight/ntzseals/inputs/ntzseals.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzseals program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzseals\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzsearchtree program.
+#echo "Building and running the \`hackers-delight/ntzsearchtree\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzsearchtree || exit
+#  cat $EXAMPLES/hackers-delight/ntzsearchtree/inputs/ntzsearchtree.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzsearchtree program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzsearchtree\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the hackers-delight/ntzsmallvals program.
+#echo "Building and running the \`hackers-delight/ntzsmallvals\` program..."
+#(
+#  cd $EXAMPLES/hackers-delight/ntzsmallvals || exit
+#  cat $EXAMPLES/hackers-delight/ntzsmallvals/inputs/ntzsmallvals.in | xargs $LEO run main || exit
+#)
+## Check that the hackers-delight/ntzsmallvals program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`hackers-delight/ntzsmallvals\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the helloworld Leo program.
+#echo "Building and running the \`helloworld\` program..."
+#(
+#  cd $EXAMPLES/helloworld || exit
+#  cat $EXAMPLES/helloworld/inputs/helloworld.in | xargs $LEO run main || exit
+#)
+## Check that the helloworld program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`helloworld\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+#
+## Build and run the interest example Leo programs.
+#echo "Building and running the \`interest\` programs..."
+#(
+#  cd $EXAMPLES/interest || exit
+#
+#  # Run the fixed period interest program.
+#  cat $EXAMPLES/interest/inputs/fixed.in | xargs $LEO run fixed_iteration_interest || exit
+#
+#  # Run the bounded period interest program.
+#  cat $EXAMPLES/interest/inputs/bounded.in | xargs $LEO run bounded_iteration_interest || exit
+#)
+## Check that the interest programs ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`interest\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
+#
+## Build and run the message example Leo program.
+#echo "Building and running the \`message\` program..."
+#(
+#  cd $EXAMPLES/message || exit
+#  cat $EXAMPLES/message/inputs/message.in | xargs $LEO run main || exit
+#)
+## Check that the message program ran successfully.
+#EXITCODE=$?
+#if [ $EXITCODE -ne 0 ]; then
+#    echo "The \`message\` program failed to run successfully."
+#    exit $EXITCODE
+#fi
 
 # Build and run the tic tac toe example Leo program.
 echo "Building and running the \`tictactoe\` program..."
 (
   cd $EXAMPLES/tictactoe || exit
   $LEO run new || exit
-  $LEO run make_move || exit
+  cat $EXAMPLES/tictactoe/inputs/tictactoe.in | xargs $LEO run make_move || exit
 
   chmod +x $EXAMPLES/tictactoe/run.sh || exit
   export -f leo
@@ -271,10 +265,10 @@ echo "Building and running the \`simple_token\` programs..."
   cd $EXAMPLES/simple_token || exit
 
   # Run the mint program.
-  $LEO run mint
+  cat $EXAMPLES/simple_token/inputs/mint.in | xargs $LEO run mint || exit
 
   # Run the transfer program.
-  $LEO run transfer
+  cat $EXAMPLES/simple_token/inputs/transfer.in | xargs $LEO run transfer || exit
 )
 # Check that the simple token programs ran successfully.
 EXITCODE=$?
@@ -288,23 +282,9 @@ echo "Building and running the \`token\` program..."
 (
   cd $EXAMPLES/token || exit
 
-  # Run the mint_public function.
-  $LEO run mint_public || exit
-
-  # Run the mint_private function.
-  $LEO run mint_private || exit
-
-  # Run the transfer_public function.
-  $LEO run transfer_public || exit
-
-  # Run the transfer_private function.
-  $LEO run transfer_private || exit
-
-  # Run the transfer_private_to_public function.
-  $LEO run transfer_private_to_public || exit
-
-  # Run the transfer_public_to_private function.
-  $LEO run transfer_public_to_private || exit
+  chmod +x $EXAMPLES/token/run.sh || exit
+  export -f leo
+  $EXAMPLES/token/run.sh || exit
 )
 # Check that the token program ran successfully.
 EXITCODE=$?
@@ -317,7 +297,7 @@ fi
 echo "Building and running the \`twoadicity\` program..."
 (
   cd $EXAMPLES/twoadicity || exit
-  $LEO run main || exit
+  cat $EXAMPLES/twoadicity/inputs/twoadicity.in | xargs $LEO run main || exit
 )
 # Check that the two-adicity program ran successfully.
 EXITCODE=$?
