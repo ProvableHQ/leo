44fcd6a4d0ab3421879d67158d2e0a6f5f98e767
d0cd <d0cd@users.noreply.github.com>
2024-02-10 06:29:43 +0000

    deploy: 2a4c98afef823684dc1b9d71d805da04bc4cc9bc

--- trait.impl/tracing_subscriber/fmt/format/trait.FormatEvent.js
+++ trait.impl/tracing_subscriber/fmt/format/trait.FormatEvent.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"leo_lang":[["impl&lt;S, N, T&gt; FormatEvent&lt;S, N&gt; for <a class=\"struct\" href=\"leo_lang/cli/helpers/logger/struct.Format.html\" title=\"struct leo_lang::cli::helpers::logger::Format\">Format</a>&lt;Full, T&gt;<span class=\"where fmt-newline\">where\n    S: Subscriber + for&lt;'a&gt; LookupSpan&lt;'a&gt;,\n    N: for&lt;'a&gt; FormatFields&lt;'a&gt; + 'static,\n    T: FormatTime,</span>"]]
+"leo_lang":[["impl&lt;S, N, T&gt; FormatEvent&lt;S, N&gt; for <a class=\"struct\" href=\"leo_lang/cli/helpers/logger/struct.Format.html\" title=\"struct leo_lang::cli::helpers::logger::Format\">Format</a>&lt;Full, T&gt;<div class=\"where\">where\n    S: Subscriber + for&lt;'a&gt; LookupSpan&lt;'a&gt;,\n    N: for&lt;'a&gt; FormatFields&lt;'a&gt; + 'static,\n    T: FormatTime,</div>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
