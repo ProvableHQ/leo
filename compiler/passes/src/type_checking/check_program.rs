69fd6edab69cde251aadb565adbca881daaee7d3
evan-schott <53463459+evan-schott@users.noreply.github.com>
2024-03-08 11:58:44 -0800

    Only track finalize stub inputs

--- compiler/passes/src/type_checking/check_program.rs
+++ compiler/passes/src/type_checking/check_program.rs
@@ -18,7 +18,7 @@ use crate::{DiGraphError, TypeChecker};
 
 use leo_ast::*;
 use leo_errors::{TypeCheckerError, TypeCheckerWarning};
-use leo_span::sym;
+use leo_span::{sym, Symbol};
 
 use snarkvm::console::network::{Network, Testnet3};
 
@@ -89,30 +89,32 @@ impl<'a> ProgramVisitor<'a> for TypeChecker<'a> {
         let scope_index = self.create_child_scope();
 
         // Create future stubs.
-        let finalize_input_map = &mut self.finalize_input_types;
-        let mut future_stubs = input.future_locations.clone();
-        let resolved_inputs = input
-            .input
-            .iter()
-            .map(|input_mode| {
-                match input_mode {
-                    Internal(function_input) => match &function_input.type_ {
-                        Future(_) => {
-                            // Since we traverse stubs in post-order, we can assume that the corresponding finalize stub has already been traversed.
-                            Future(FutureType::new(
-                                finalize_input_map.get(&future_stubs.pop().unwrap()).unwrap().clone(),
-                            ))
-                        }
-                        _ => function_input.clone().type_,
-                    },
-                    External(_) => unreachable!("External inputs are not allowed in finalize outputs of stubs."),
-                }
-            })
-            .collect();
-        assert!(future_stubs.is_empty(), "Disassembler produced malformed stub.");
+        if input.variant == Variant::Standard && input.is_async {
+            let finalize_input_map = &mut self.finalize_input_types;
+            let mut future_stubs = input.future_locations.clone();
+            let resolved_inputs:Vec<Type> = input
+                .input
+                .iter()
+                .map(|input_mode| {
+                    match input_mode {
+                        Internal(function_input) => match &function_input.type_ {
+                            Future(_) => {
+                                // Since we traverse stubs in post-order, we can assume that the corresponding finalize stub has already been traversed.
+                                Future(FutureType::new(
+                                    finalize_input_map.get(&future_stubs.pop().unwrap()).unwrap().clone(),
+                                ))
+                            }
+                            _ => function_input.clone().type_,
+                        },
+                        External(_) => unreachable!("External inputs are not allowed in finalize outputs of stubs."),
+                    }
+                })
+                .collect();
+            assert!(future_stubs.is_empty(), "Disassembler produced malformed stub.");
 
-        finalize_input_map
-            .insert(Location::new(self.scope_state.program_name.unwrap(), input.identifier.name), resolved_inputs);
+            finalize_input_map
+                .insert(Location::new(self.scope_state.program_name.unwrap(), input.identifier.name), resolved_inputs);
+        }
 
         // Query helper function to type check function parameters and outputs.
         self.check_function_signature(&Function::from(input.clone()));
