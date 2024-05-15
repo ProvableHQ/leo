045af83d228334219b2b4601f7898c36a67bc467
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2024-05-15 12:33:59 -0700

    Cleanup

--- leo/cli/commands/mod.rs
+++ leo/cli/commands/mod.rs
@@ -58,13 +58,13 @@ use crate::cli::helpers::context::*;
 use leo_errors::{emitter::Handler, CliError, PackageError, Result};
 use leo_package::{build::*, outputs::OutputsDirectory, package::*};
 use snarkvm::prelude::{block::Transaction, Ciphertext, Plaintext, PrivateKey, Record, ViewKey};
+use snarkvm::{console::network::Network};
 
 use clap::Parser;
 use colored::Colorize;
 use std::str::FromStr;
 use tracing::span::Span;
 
-use snarkvm::{console::network::Network, prelude::ToBytes};
 
 /// Base trait for the Leo CLI, see methods and their documentation for details.
 pub trait Command {
@@ -212,15 +212,6 @@ fn handle_broadcast<N: Network>(endpoint: &String, transaction: Transaction<N>, 
     println!("Broadcasting transaction to {}...", endpoint.clone());
     // Get the transaction id.
     let transaction_id = transaction.id();
-
-    // TODO: remove
-    println!("Transaction {:?}", transaction);
-    let tx_bytes = transaction.to_bytes_le()?;
-    println!("Transaction bytes: {:?}", tx_bytes);
-    let tx_json = serde_json::to_string(&transaction).unwrap();
-    println!("Transaction JSON: {:?}", tx_json);
-    let deserialize_tx_json = serde_json::from_str::<Transaction<N>>(&tx_json).unwrap();
-    println!("Deserialized transaction: {:?}", deserialize_tx_json);
 
     // Send the deployment request to the local development node.
     return match ureq::post(endpoint).send_json(&transaction) {
