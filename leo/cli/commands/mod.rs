e739bdba42cdf2532bc3a305930d0156db262380
Zk <zklim95@hotmail.com>
2024-06-08 01:23:46 +0800

    fix(API): API should be https

--- leo/cli/commands/mod.rs
+++ leo/cli/commands/mod.rs
@@ -128,7 +128,7 @@ pub trait Command {
 /// require Build command output as their input.
 #[derive(Parser, Clone, Debug, Default)]
 pub struct BuildOptions {
-    #[clap(long, help = "Endpoint to retrieve network state from.", default_value = "http://api.explorer.aleo.org/v1")]
+    #[clap(long, help = "Endpoint to retrieve network state from.", default_value = "https://api.explorer.aleo.org/v1")]
     pub endpoint: String,
     #[clap(long, help = "Does not recursively compile dependencies.")]
     pub non_recursive: bool,
