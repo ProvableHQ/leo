a6eee8b927f632ec9e7d41be3048301f3b50976b
mikebenfield <mikebenfield@users.noreply.github.com>
2025-01-31 18:39:21 +0000

    deploy: d22d7aa9e8c19a16e6eb9ed940791bf8cadfc2e6

--- trait.impl/core/ops/arith/trait.Sub.js
+++ trait.impl/core/ops/arith/trait.Sub.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_span",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"leo_span/span/struct.BytePos.html\" title=\"struct leo_span::span::BytePos\">BytePos</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"leo_span/span/struct.CharPos.html\" title=\"struct leo_span::span::CharPos\">CharPos</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_span",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"leo_span/span/struct.BytePos.html\" title=\"struct leo_span::span::BytePos\">BytePos</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"leo_span/span/struct.CharPos.html\" title=\"struct leo_span::span::CharPos\">CharPos</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_interpreter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"leo_interpreter/ratatui_ui/struct.RatatuiUi.html\" title=\"struct leo_interpreter::ratatui_ui::RatatuiUi\">RatatuiUi</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_interpreter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"leo_interpreter/ratatui_ui/struct.RatatuiUi.html\" title=\"struct leo_interpreter::ratatui_ui::RatatuiUi\">RatatuiUi</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
