de59e6b19d043078ca128dfce669cee28f9fe192
mikebenfield <mikebenfield@users.noreply.github.com>
2025-02-18 18:20:58 +0000

    deploy: c968579857ee85a19c5929a8e99bbb95335e8df8

--- trait.impl/clap_builder/derive/trait.Subcommand.js
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_lang",[["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/cli/enum.Commands.html\" title=\"enum leo_lang::cli::cli::Commands\">Commands</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/account/enum.Account.html\" title=\"enum leo_lang::cli::commands::account::Account\">Account</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/example/enum.ExampleVariant.html\" title=\"enum leo_lang::cli::commands::example::ExampleVariant\">ExampleVariant</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/query/enum.QueryCommands.html\" title=\"enum leo_lang::cli::commands::query::QueryCommands\">QueryCommands</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_lang",[["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/cli/enum.Commands.html\" title=\"enum leo_lang::cli::cli::Commands\">Commands</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/account/enum.Account.html\" title=\"enum leo_lang::cli::commands::account::Account\">Account</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/query/enum.QueryCommands.html\" title=\"enum leo_lang::cli::commands::query::QueryCommands\">QueryCommands</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[697]}
+//{"start":57,"fragment_lengths":[508]}
