d2b5d8e8cf1c8031b52f76bc1bfd1a4ba290a64d
mikebenfield <mikebenfield@users.noreply.github.com>
2025-03-06 19:52:27 +0000

    deploy: 258a3784af84caf0676eacda956fbb26bda35de3

--- trait.impl/clap_builder/derive/trait.Subcommand.js
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_lang",[["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/cli/enum.Commands.html\" title=\"enum leo_lang::cli::cli::Commands\">Commands</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/account/enum.Account.html\" title=\"enum leo_lang::cli::commands::account::Account\">Account</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/commands/query/enum.QueryCommands.html\" title=\"enum leo_lang::cli::commands::query::QueryCommands\">QueryCommands</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_lang",[["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/cli/enum.Commands.html\" title=\"enum leo_lang::cli::cli::Commands\">Commands</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/enum.Account.html\" title=\"enum leo_lang::cli::Account\">Account</a>"],["impl Subcommand for <a class=\"enum\" href=\"leo_lang/cli/query/enum.QueryCommands.html\" title=\"enum leo_lang::cli::query::QueryCommands\">QueryCommands</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[508]}
+//{"start":57,"fragment_lengths":[453]}
--- trait.impl/tracing_subscriber/fmt/format/trait.FormatEvent.js
+++ trait.impl/tracing_subscriber/fmt/format/trait.FormatEvent.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_lang",[["impl&lt;S, N, T&gt; FormatEvent&lt;S, N&gt; for <a class=\"struct\" href=\"leo_lang/cli/helpers/logger/struct.Format.html\" title=\"struct leo_lang::cli::helpers::logger::Format\">Format</a>&lt;Full, T&gt;<div class=\"where\">where\n    S: Subscriber + for&lt;'a&gt; LookupSpan&lt;'a&gt;,\n    N: for&lt;'a&gt; FormatFields&lt;'a&gt; + 'static,\n    T: FormatTime,</div>"]]]]);
+    var implementors = Object.fromEntries([["leo_lang",[["impl&lt;S, N, T&gt; FormatEvent&lt;S, N&gt; for <a class=\"struct\" href=\"leo_lang/cli/logger/struct.Format.html\" title=\"struct leo_lang::cli::logger::Format\">Format</a>&lt;Full, T&gt;<div class=\"where\">where\n    S: Subscriber + for&lt;'a&gt; LookupSpan&lt;'a&gt;,\n    N: for&lt;'a&gt; FormatFields&lt;'a&gt; + 'static,\n    T: FormatTime,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[389]}
+//{"start":57,"fragment_lengths":[372]}
