bbf30cbc2bd9ceb50c15780c99fc06b24a01ca58
mikebenfield <mikebenfield@users.noreply.github.com>
2025-03-21 15:38:02 +0000

    deploy: 79e645ca33918582d30300658b44c685f972419d

--- trait.impl/serde/de/trait.Visitor.js
+++ trait.impl/serde/de/trait.Visitor.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_span",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.214/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for <a class=\"struct\" href=\"leo_span/span_json/struct.SpanMapVisitor.html\" title=\"struct leo_span::span_json::SpanMapVisitor\">SpanMapVisitor</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_span",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for <a class=\"struct\" href=\"leo_span/span_json/struct.SpanMapVisitor.html\" title=\"struct leo_span::span_json::SpanMapVisitor\">SpanMapVisitor</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
