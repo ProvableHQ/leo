5bafb15bb4833f220d787a0c964dfc9dde43b279
d0cd <d0cd@users.noreply.github.com>
2025-05-01 15:16:18 +0000

    deploy: cf70073f0edc311f237eec514efc2b4d66a76426

--- trait.impl/leo_ast/passes/consumer/trait.ProgramConsumer.js
+++ trait.impl/leo_ast/passes/consumer/trait.ProgramConsumer.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_passes",[["impl ProgramConsumer for <a class=\"struct\" href=\"leo_passes/static_single_assignment/visitor/struct.SsaFormingVisitor.html\" title=\"struct leo_passes::static_single_assignment::visitor::SsaFormingVisitor\">SsaFormingVisitor</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["leo_passes",[["impl <a class=\"trait\" href=\"leo_ast/passes/consumer/trait.ProgramConsumer.html\" title=\"trait leo_ast::passes::consumer::ProgramConsumer\">ProgramConsumer</a> for <a class=\"struct\" href=\"leo_passes/static_single_assignment/visitor/struct.SsaFormingVisitor.html\" title=\"struct leo_passes::static_single_assignment::visitor::SsaFormingVisitor\">SsaFormingVisitor</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[262]}
+//{"start":57,"fragment_lengths":[404]}
