028c1f07f8c872ec215e8bff5360759a128fda48
mohammadfawaz <mohammadfawaz@users.noreply.github.com>
2025-05-16 16:28:19 +0000

    deploy: fb600537d21c96180ba3fa50fcc6019806393cd7

--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_package",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"leo_package/enum.NetworkName.html\" title=\"enum leo_package::NetworkName\">NetworkName</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_package",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"leo_package/enum.NetworkName.html\" title=\"enum leo_package::NetworkName\">NetworkName</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
