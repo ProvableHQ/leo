03319703333ea2ffb0353cfd931814eb9dbe03c1
Alex Cameron <asc@tetsuo.sh>
2025-06-27 12:17:18 +1000

    Ensure that programs are added to the VM as we go

--- leo/cli/commands/deploy.rs
+++ leo/cli/commands/deploy.rs
@@ -205,25 +205,23 @@ fn handle_deploy<N: Network>(
             // Print the deployment stats.
             print_deployment_stats(&program_id.to_string(), deployment, priority_fee)?;
             // Save the transaction.
-            transactions.push((program, transaction));
+            transactions.push((program_id, transaction));
         }
-    }
-
-    for (program, transaction) in transactions.iter() {
-        // Validate the deployment limits.
-        let deployment = transaction.deployment().expect("Expected a deployment in the transaction");
-        validate_deployment_limits(deployment, &program.id(), &network)?;
-
         // Add the program to the VM.
         vm.process().write().add_program(&program)?;
+    }
+
+    for (program_id, transaction) in transactions.iter() {
+        // Validate the deployment limits.
+        let deployment = transaction.deployment().expect("Expected a deployment in the transaction");
+        validate_deployment_limits(deployment, &program_id, &network)?;
     }
 
     // If the `print` option is set, print the deployment transaction to the console.
     // The transaction is printed in JSON format.
     if command.action.print {
-        for (program, transaction) in transactions.iter() {
+        for (program_name, transaction) in transactions.iter() {
             // Pretty-print the transaction.
-            let program_name = program.id();
             let transaction_json = serde_json::to_string_pretty(transaction)
                 .map_err(|e| CliError::custom(format!("Failed to serialize transaction: {e}")))?;
             println!("🖨️ Printing deployment for {program_name}\n{transaction_json}")
@@ -236,8 +234,7 @@ fn handle_deploy<N: Network>(
     if let Some(path) = &command.action.save {
         // Create the directory if it doesn't exist.
         std::fs::create_dir_all(path).map_err(|e| CliError::custom(format!("Failed to create directory: {e}")))?;
-        for (program, transaction) in transactions.iter() {
-            let program_name = program.id();
+        for (program_name, transaction) in transactions.iter() {
             // Save the transaction to a file.
             let file_path = PathBuf::from(path).join(format!("{program_name}.deployment.json"));
             println!("💾 Saving deployment for {program_name} at {}", file_path.display());
@@ -250,8 +247,7 @@ fn handle_deploy<N: Network>(
 
     // If the `broadcast` option is set, broadcast each deployment transaction to the network.
     if command.action.broadcast {
-        for (i, (program, transaction)) in transactions.iter().enumerate() {
-            let program_id = program.id();
+        for (i, (program_id, transaction)) in transactions.iter().enumerate() {
             println!("\n📡 Broadcasting deployment for {}...", program_id.to_string().bold());
             // Get and confirm the fee with the user.
             let fee = transaction.fee_transition().expect("Expected a fee in the transaction");
