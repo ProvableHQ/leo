172fcb566b92c5101472512ef15d4eea3a2554b7
mikebenfield <mikebenfield@users.noreply.github.com>
2025-06-28 00:15:25 +0000

    deploy: 72c6bebf5266f766a1cec6f2641e6eed61549e28

--- trait.impl/core/fmt/trait.Write.js
+++ trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_ast",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"leo_ast/indent_display/struct.IndentWriter.html\" title=\"struct leo_ast::indent_display::IndentWriter\">IndentWriter</a>&lt;'_, '_&gt;"]]]]);
+    var implementors = Object.fromEntries([["leo_ast",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"leo_ast/indent_display/struct.IndentWriter.html\" title=\"struct leo_ast::indent_display::IndentWriter\">IndentWriter</a>&lt;'_, '_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
