b53797cba863b30c19bbf88901e015a363ec697f
berZKerk <gutsberzkerk@gmail.com>
2025-08-22 16:20:43 +0200

    Update errors/src/errors/utils/util_errors.rs
    Co-authored-by: Michael Benfield <mike.benfield@gmail.com>
    Signed-off-by: berZKerk <gutsberzkerk@gmail.com>

--- errors/src/errors/utils/util_errors.rs
+++ errors/src/errors/utils/util_errors.rs
@@ -58,7 +58,7 @@ create_messages!(
     program_size_limit_exceeded {
         args: (name: impl Display, size: usize, limit: usize),
         msg: format!("Program `{name}.aleo` exceeds the maximum size limit. Program size: {size} bytes; maximum allowed: {limit} bytes."),
-        help: Some("Reduce the program size by removing unnecessary code, optimizing functions, or splitting the program into smaller modules.".to_string()),
+        help: Some("Reduce the program size by removing unnecessary code, optimizing functions, or splitting the program into smaller programs.".to_string()),
     }
 
     @backtraced
