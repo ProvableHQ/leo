b04811ed9add556b90659471932135b7894a9434
tetektoza <tetektoza@gmail.com>
2025-10-03 00:12:00 +0200

    Use absolute Path in SSA

--- compiler/passes/src/static_single_assignment/expression.rs
+++ compiler/passes/src/static_single_assignment/expression.rs
@@ -54,7 +54,7 @@ impl SsaFormingVisitor<'_> {
         } else {
             let (place, statement) = self.unique_simple_definition(expr);
             statements.push(statement);
-            (Path::from(place).into(), statements)
+            (Path::from(place).with_absolute_path(Some(vec![place.name])).into(), statements)
         }
     }
 }
@@ -159,7 +159,9 @@ impl ExpressionConsumer for SsaFormingVisitor<'_> {
                 let (expression, mut stmts) = if let Some(expr) = arg.expression {
                     self.consume_expression_and_define(expr)
                 } else {
-                    self.consume_path(Path::from(arg.identifier))
+                    self.consume_path(
+                        Path::from(arg.identifier).with_absolute_path(Some(vec![arg.identifier.name]))
+                    )
                 };
                 // Accumulate any statements produced.
                 statements.append(&mut stmts);
