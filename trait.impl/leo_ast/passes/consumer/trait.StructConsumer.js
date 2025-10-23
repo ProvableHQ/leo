5ef1452f9f7226e9022a6b6b1716a2cdd901507f
mohammadfawaz <mohammadfawaz@users.noreply.github.com>
2025-10-23 17:06:09 +0000

    deploy: d83763329b97e34f8f4a80f8654923c62215af0f

--- trait.impl/leo_ast/passes/consumer/trait.StructConsumer.js
+++ trait.impl/leo_ast/passes/consumer/trait.StructConsumer.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_passes",[["impl <a class=\"trait\" href=\"leo_ast/passes/consumer/trait.StructConsumer.html\" title=\"trait leo_ast::passes::consumer::StructConsumer\">StructConsumer</a> for <a class=\"struct\" href=\"leo_passes/static_single_assignment/visitor/struct.SsaFormingVisitor.html\" title=\"struct leo_passes::static_single_assignment::visitor::SsaFormingVisitor\">SsaFormingVisitor</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["leo_passes",[["impl <a class=\"trait\" href=\"leo_ast/passes/consumer/trait.StructConsumer.html\" title=\"trait leo_ast::passes::consumer::StructConsumer\">StructConsumer</a> for <a class=\"struct\" href=\"leo_passes/visitor/struct.SsaFormingVisitor.html\" title=\"struct leo_passes::visitor::SsaFormingVisitor\">SsaFormingVisitor</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[401]}
+//{"start":57,"fragment_lengths":[350]}
