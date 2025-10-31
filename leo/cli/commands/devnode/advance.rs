2f55dad8c72166ac76a5e90e6daea46056ef5b22
Roee-87 <roy@provable.com>
2025-10-31 15:47:17 -0700

    added command for advancing blocks

---
+++ leo/cli/commands/devnode/advance.rs
@@ -0,0 +1,74 @@
+// Copyright (C) 2019-2025 Provable Inc.
+// This file is part of the Leo library.
+
+// The Leo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The Leo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
+
+use super::*;
+
+// Advance the devnode ledger by a specified number of blocks.  The default value is 1.
+#[derive(Parser, Debug)]
+pub struct Advance{
+    #[clap(help = "The number of blocks to advance the ledger by", default_value = "1")]
+    pub num_blocks: u32,
+}
+
+impl Command for Advance {
+    type Input = ();
+    type Output = ();
+
+    fn log_span(&self) -> Span {
+        tracing::span!(tracing::Level::INFO, "Leo")
+    }
+
+    fn prelude(&self, _context: Context) -> Result<Self::Input> {
+        Ok(())
+    }
+
+    fn apply(self, context: Context, _: Self::Input) -> Result<Self::Output> {
+        let network: NetworkName = get_network(&context.env_override.network)?;
+        let endpoint = get_endpoint(&context.env_override.endpoint)?;
+        handle_advance_devnode(context, network, &endpoint, self.num_blocks)
+    }
+}
+
+fn handle_advance_devnode(
+    context: Context,
+    network: NetworkName,
+    endpoint: &str,
+    num_blocks: u32,
+) -> Result<()> {
+    tracing::info!(
+        "Advancing the devnode ledger by {} block(s) on network '{}' at endpoint '{}'",
+        num_blocks,
+        network,
+        endpoint
+    );
+
+    for i in 0..num_blocks {
+        // Call the REST API to advance the ledger by one block.
+        let client = reqwest::blocking::Client::new();
+        let payload = json!({
+            private_key: context.env_override.private_key,
+        });
+
+        let response = client
+            .post("http://localhost:3030/testnet/block/create")
+            .header("Content-Type", "application/json")
+            .json(&payload)
+            .send()
+            .await?;
+    }
+
+    Ok(())
+}
--- leo/cli/commands/devnode/mod.rs
+++ leo/cli/commands/devnode/mod.rs
@@ -18,6 +18,20 @@ pub mod logger;
 pub mod rest;
 pub mod start;
 
+#[derive(Parser, Debug)]
+pub enum DevnodeCommands{
+    #[clap(name = "start", about = "Start the devnode")]
+    Start {
+        #[clap(flatten)]
+        command: start::start_devnode;,
+    },
+    #[clap(name = "advance", about = "Advance the ledger by a specified number of blocks")]
+    Advance {
+        #[clap(flatten)]
+        command: advance::advance_devnode;,
+    },
+}
+
 ///  Query live data from the Aleo network.
 #[derive(Parser, Debug)]
 pub struct LeoDevnode {
@@ -25,4 +39,50 @@ pub struct LeoDevnode {
     pub(crate) env_override: EnvOptions,
     #[clap(subcommand)]
     pub command: DevnodeCommands,
+}
+
+impl Command for LeoDevnode {
+    type Input = ();
+    type Output = String;
+
+    fn log_span(&self) -> Span {
+        tracing::span!(tracing::Level::INFO, "Leo")
+    }
+
+    fn prelude(&self, _context: Context) -> Result<Self::Input> {
+        Ok(())
+    }
+
+    fn apply(self, context: Context, _: Self::Input) -> Result<Self::Output> {
+        // Parse the network.
+        let network: NetworkName = get_network(&self.env_override.network)?;
+        let endpoint = get_endpoint(&self.env_override.endpoint)?;
+        hanlde_devnode(self, context, network, &endpoint)
+    }
+}
+
+// A helper function to handle the devnode command based on the subcommand provided.
+fn hanlde_devnode(
+    devnode_command: LeoDevnode,
+    context: Context,
+    network: NetworkName,
+    endpoint: &str,
+) -> Result<<LeoDevnode as Command>::Output> {
+    let recursive = context.recursive;
+    match devnode_command.command {
+        DevnodeCommands::Start { command } => {
+            if endpoint != "http://localhost:3030" {
+                tracing::warn!(
+                    "⚠️  Using a custom endpoint for the devnode: {}. This may lead to unexpected behavior.",
+                    endpoint
+                );
+            }
+            tracing::info!("Starting the devnode on network: {} with endpoint: {}", network, endpoint);
+            command.apply(context, ())
+        }
+        DevnodeCommands::Advance { command } => {
+            tracing::info!("Advancing the devnode on network: {} with endpoint: {}", network, endpoint);
+            command.apply(context, ())
+        }
+    }
 }
