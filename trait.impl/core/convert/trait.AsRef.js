56aaf7c30d11ac7945740f7b2eb30cb5bd642da7
IGI-111 <IGI-111@users.noreply.github.com>
2025-11-12 16:25:38 +0000

    deploy: c75eb40e6f352719135bbdb70b10af32b22f4939

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_ast",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"leo_ast/struct.Program.html\" title=\"struct leo_ast::Program\">Program</a>&gt; for <a class=\"struct\" href=\"leo_ast/struct.Ast.html\" title=\"struct leo_ast::Ast\">Ast</a>"]]]]);
+    var implementors = Object.fromEntries([["leo_ast",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"leo_ast/struct.Program.html\" title=\"struct leo_ast::Program\">Program</a>&gt; for <a class=\"struct\" href=\"leo_ast/struct.Ast.html\" title=\"struct leo_ast::Ast\">Ast</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["leo_parser_lossless",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"leo_parser_lossless/tokens/struct.Lexer.html\" title=\"struct leo_parser_lossless::tokens::Lexer\">Lexer</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["leo_parser_lossless",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"leo_parser_lossless/tokens/struct.Lexer.html\" title=\"struct leo_parser_lossless::tokens::Lexer\">Lexer</a>&lt;'a&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
