8f22a5f49ce194e376bd41fbe13182adf979624e
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2025-12-17 01:42:09 -0500

    Upgrade Rust version and remove some dead files (#29042)
    Co-authored-by: Mohammad Fawaz <mohammadfawaz@gmail.com>

--- errors/src/errors/flattener/flattener_errors.rs
+++ errors/src/errors/flattener/flattener_errors.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the Leo library. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::create_messages;
 use std::fmt::{Debug, Display};
 
 create_messages!(
