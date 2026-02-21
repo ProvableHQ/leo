e64a8c11ab9fbc40e8471666050add7b5042cfea
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2026-02-21 10:30:33 -0500

    Cherry-pick https://github.com/ProvableHQ/leo/pull/29141 (#29152)
    Co-authored-by: Mohammad Fawaz <mohammadfawaz@gmail.com>

--- leo/cli/commands/upgrade.rs
+++ leo/cli/commands/upgrade.rs
@@ -303,13 +303,9 @@ fn handle_upgrade<N: Network, A: Aleo<Network = N>>(
     }
     println!();
 
-    // Remove version suffixes from the endpoint.
-    let re = regex::Regex::new(r"v\d+$").unwrap();
-    let query_endpoint = re.replace(&endpoint, "").to_string();
-
     // Specify the query.
     let query = SnarkVMQuery::<N, BlockMemory<N>>::from(
-        query_endpoint
+        endpoint
             .parse::<Uri>()
             .map_err(|e| CliError::custom(format!("Failed to parse endpoint URI '{endpoint}': {e}")))?,
     );
