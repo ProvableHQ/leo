d24b0fd1fb414c6e76c397d1647d1c5e062d3f14
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2026-03-04 11:07:59 -0500

    Improve version information

--- crates/leo/src/cli/main.rs
+++ crates/leo/src/cli/main.rs
@@ -30,9 +30,10 @@ fn set_panic_hook() {
                 "note: we would appreciate a bug report: https://github.com/ProvableHQ/leo/issues/new?labels=bug,panic&template=bug.md&title=[Bug]\n"
             );
             eprintln!(
-                "note: {} {} running on {} {}\n",
+                "note: {} {} ({}) running on {} {}\n",
                 env!("CARGO_PKG_NAME"),
                 env!("CARGO_PKG_VERSION"),
+                env!("LEO_GIT_HASH"),
                 sys_info::os_type().unwrap_or_else(|e| e.to_string()),
                 sys_info::os_release().unwrap_or_else(|e| e.to_string()),
             );
