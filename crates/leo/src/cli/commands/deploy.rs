1fe72c8e07f5d40692abec7a8e30ba68f712a9f4
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2026-03-27 12:46:14 +0100

    Enable leo deploy with dummy record translation vks

--- crates/leo/src/cli/commands/deploy.rs
+++ crates/leo/src/cli/commands/deploy.rs
@@ -711,11 +711,16 @@ pub(crate) fn deploy_with_placeholder_certificate<N: Network, A: Aleo<Network = 
 ) -> Result<(Transaction<N>, DeploymentStats)> {
     assert!(!program.functions().is_empty(), "Program `{}` has no functions", program.id());
     // Initialize a vector for the placeholder verifying keys and certificates.
-    let mut verifying_keys = Vec::with_capacity(program.functions().len());
+    let mut verifying_keys = Vec::with_capacity(program.functions().len() + program.records().len());
     for function_name in program.functions().keys() {
         let verifying_key = VerifyingKey::from_str(leo_compiler::run::PLACEHOLDER_VK)?;
         let certificate = Certificate::from_str(leo_compiler::run::PLACEHOLDER_CERT)?;
         verifying_keys.push((*function_name, (verifying_key, certificate)));
+    }
+    for record_name in program.records().keys() {
+        let verifying_key = VerifyingKey::from_str(leo_compiler::run::PLACEHOLDER_VK)?;
+        let certificate = Certificate::from_str(leo_compiler::run::PLACEHOLDER_CERT)?;
+        verifying_keys.push((*record_name, (verifying_key, certificate)));
     }
     // Create the deployment.
     let mut deployment = Deployment::new(edition, program.clone(), verifying_keys, None, None).unwrap();
