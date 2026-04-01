f93ae101fce760039eda69b4f935afc33f24e790
IGI-111 <igi-111@protonmail.com>
2026-04-01 21:22:18 +0200

    Fix compiler panic
    Fixes #29224
    
    Return proper error type preventing fetching a finalization that does
    not exist.

--- crates/passes/src/type_checking/ast.rs
+++ crates/passes/src/type_checking/ast.rs
@@ -1135,6 +1135,7 @@ impl AstVisitor for TypeCheckingVisitor<'_> {
         if self.async_block_id.is_some() && !matches!(func.variant, Variant::FinalFn | Variant::Fn) {
             // FIXME better error
             self.emit_err(TypeCheckerError::can_only_call_inline_function("a final block", input.span));
+            return Type::Err;
         }
 
         // Async functions return a single future.
